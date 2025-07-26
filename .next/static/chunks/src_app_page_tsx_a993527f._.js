e83ff4bf781081098c3d067a829a48c4a58e44ba
Bhgya2904 <147989431+Bhgya2904@users.noreply.github.com>
2025-07-26 18:10:56 +0530

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1753533655790

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,13 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_react-leaflet_lib_index_7c597eb1.js",
+  "static/chunks/src_a0cb3c72._.js",
+  "static/chunks/node_modules_axios_lib_99999129._.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_leaflet_dist_leaflet-src_e7e140e9.js",
+  "static/chunks/node_modules_8343dc24._.js",
+  "static/chunks/node_modules_leaflet_dist_leaflet_88e19fd3.css"
+],
+    source: "dynamic"
+});
